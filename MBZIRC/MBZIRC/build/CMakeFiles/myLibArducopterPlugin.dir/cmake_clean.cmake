b9698e2de12f8fbfea83dad5e5bd54161806450d
Alfredo Bochicchio <36285257+Bochicchio3@users.noreply.github.com>
2019-03-25 08:16:10 -0700

    Add files via upload

---
+++ MBZIRC/MBZIRC/build/CMakeFiles/myLibArducopterPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myLibArducopterPlugin.dir/ArduCopterPlugin.cc.o"
+  "libmyLibArducopterPlugin.pdb"
+  "libmyLibArducopterPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myLibArducopterPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
